2a732581c58f07908501eae036fd48c1513c8672
eunjae.kim <eunjaekim@linecorp.com>
2020-06-28 14:34:56 +0900

    Apply express

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('hi');
+const express = require('express')
+const app = express()
+
+const PORT = 4000
+
+const handleListening = () => {
+  console.log(`Listening on: http://localhost:${PORT}`)
+}
+
+const handleHome = (req, res) => {
+  res.send('Hello from home');
+}
+
+const handleProfile = (req, res) => {
+  res.send("You are on my profile")
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
