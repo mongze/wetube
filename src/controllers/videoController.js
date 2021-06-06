0d64d60d4f9fea0c425944950d7a1a1fb7dad06a
eunjae.kim <eunjae1105@gmail.com>
2021-06-07 08:40:31 +0900

    #6.13 Async Await

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,34 +1,8 @@
-// import { videos } from '../db';
+import Video from '../models/Video';
 
-let videos = [
-  {
-    title: 'First Video',
-    rating: 5,
-    comments: 2,
-    createdAt: '2 minutes ago',
-    views: 59,
-    id: 1,
-  },
-  {
-    title: 'Second Video',
-    rating: 5,
-    comments: 2,
-    createdAt: '2 minutes ago',
-    views: 1,
-    id: 2,
-  },
-  {
-    title: 'Third Video',
-    rating: 5,
-    comments: 2,
-    createdAt: '2 minutes ago',
-    views: 59,
-    id: 3,
-  },
-];
-
-export const home = (req, res) => {
-  return res.render('home', { pageTitle: 'Home', videos });
+export const home = async (req, res) => {
+  const vidoes = await Video.find({});
+  return res.render('home', { pageTitle: 'Home', vidoes: [] });
 };
 
 export const search = (req, res) => {
@@ -40,19 +14,16 @@ export const search = (req, res) => {
 
 export const watch = (req, res) => {
   const { id } = req.params; // const id = req.params.id;
-  const video = videos[id - 1];
-  return res.render('watch', { pageTitle: `Watching: ${video.title}`, video });
+  return res.render('watch', { pageTitle: `Watching` });
 };
 
 export const getEdit = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
-  return res.render('editVideo', { pageTitle: `Editing: ${video.title}`, video });
+  return res.render('editVideo', { pageTitle: `Editing` });
 };
 
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
-  video[id - 1].title = title;
   return res.redirect(`/videos/${id}`);
 };
--- src/init.js
+++ src/init.js
@@ -1,8 +1,8 @@
+import './db';
+import Video from './models/Video';
 import app from './app';
 
 const PORT = 4000;
-
-console.log(process.cwd());
 
 const handleListening = () => console.log(`✅ Listening on http://localhost:${PORT}`);
 
