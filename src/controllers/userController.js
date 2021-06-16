dec7d110564f92d624b4af14966696b2694f031d
eunjae.kim <eunjae1105@gmail.com>
2021-06-17 00:30:21 +0900

    #7.21 Github Login (existing user)

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -101,15 +101,29 @@ export const finishGithubLogin = async (req, res) => {
         },
       })
     ).json();
-    console.log('✅', userData);
-    console.log('✅', emailData);
 
-    const email = emailData.find((email) => email.primary === true && email.verified === true);
-
-    if (!email) {
+    const emailObj = emailData.find((email) => email.primary === true && email.verified === true);
+    if (!emailObj) {
       return res.redirect('/login');
     }
-    // 중복된 email 처리
+    const existingUser = await User.findOne({ email: emailObj.email });
+    if (existingUser) {
+      req.session.loggedIn = true;
+      req.session.user = existingUser;
+      return res.redirect('/');
+    } else {
+      const user = await User.create({
+        name: userData.name,
+        username: userData.login,
+        email: emailObj.email,
+        password: '',
+        socialOnly: true,
+        location: userData.location,
+      });
+      req.session.loggedIn = true;
+      req.session.user = user;
+      return res.redirect('/');
+    }
   } else {
     return res.redirect('/login'); // error message 혹은 noti 주면 좋겠네
   }
