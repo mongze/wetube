de9923e6d62f25089aacdf6d272bce52438025c7
eunjae.kim <eunjae1105@gmail.com>
2021-06-24 23:52:17 +0900

    #13.1 Video Preview

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,8 +1,10 @@
 const startBtn = document.getElementById('startBtn');
+const video = document.getElementById('preview');
 
 const handleStart = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
-  console.log(stream);
+  const stream = await navigator.mediaDevices.getUserMedia({ audio: false, video: true });
+  video.srcObject = stream; // src 파일이 아니니까!
+  video.play();
 };
 
 startBtn.addEventListener('click', handleStart);
